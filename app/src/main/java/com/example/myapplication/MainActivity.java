cd2b251fc37c6a937bc493581636905b533306e6
KovacZan <39158639+KovacZan@users.noreply.github.com>
2019-06-09 11:01:59 +0200

    test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,8 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import org.arkecosystem.crypto.configuration.Network;
+import org.arkecosystem.crypto.identities.Address;
+import org.arkecosystem.crypto.identities.PublicKey;
+import org.arkecosystem.crypto.identities.WIF;
+
 import java.io.IOException;
+import java.security.SecureRandom;
 import java.security.spec.ECField;
+
+import io.github.novacrypto.bip39.MnemonicGenerator;
+import io.github.novacrypto.bip39.Words;
+import io.github.novacrypto.bip39.wordlists.English;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,17 +29,33 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Network.set(new ParkNet());
+                try {
+                    StringBuilder sb = new StringBuilder();
+                    byte[] entropy = new byte[Words.TWELVE.byteLength()];
+                    new SecureRandom().nextBytes(entropy);
+                    new MnemonicGenerator(English.INSTANCE)
+                            .createMnemonic(entropy, sb::append);
+                    System.out.println("My passphrase: " + sb.toString());
+                    System.out.println("Address: " + Address.fromPassphrase(sb.toString(), 55));
+                    System.out.println("Public key: " + PublicKey.fromPassphrase(sb.toString()));
 
-                    new Thread()
-                    {
-                        public void run() {
-                            try {
-                                ParkTransaction.CreateParkTransaction(10000, "PFfkYKD8uvXk7M7FY6kBTZdeWLpRcHABp3", "memory average vital away document demise nest glow mosquito mammal hold replace");
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }.start();
+                    // System.out.println("To je: "+WIF.fromPassphrase("project eternal damage spare grant tube summer slot patrol unaware fossil exposeeeeer"));
+                    //System.out.println(Address.validate(Address.fromPassphrase("project eternael damage spare grant tube summer slot patrol unaware fossil exprt", 55), 55));
+                    //System.out.println("Address: " + Address.fromPassphrase("project eternael damage spare grant tube summer slot patrol unaware fossil exprt",55));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                //new Thread()
+                    //{
+                    //    public void run() {
+                    //        try {
+                    //            ParkTransaction.CreateParkTransaction(10000, "PFfkYKD8uvXk7M7FY6kBTZdeWLpRcHABp3", "memory average vital away document demise nest glow mosquito mammal hold replace");
+                    //        } catch (IOException e) {
+                    //            e.printStackTrace();
+                    //        }
+                    //    }
+                    //}.start();
 
 
             }
